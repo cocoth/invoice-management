3d190f848be0e782e203304bf61fa9b6ac89b244
cocoth <anonymousid0000000@gmail.com>
2025-10-04 22:02:49 +0700

    add: tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,6 +81,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head>
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NP8NQZSL');`
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
@@ -89,6 +100,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased transition-colors duration-300`}
       >
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe 
+        src="https://www.googletagmanager.com/ns.html?id=GTM-NP8NQZSL"
+        height="0" 
+        width="0" 
+        style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -97,7 +118,7 @@ export default function RootLayout({
           enableColorScheme
         >
           {children}
-          <Analytics/>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
